a6346e6c6cf5d0896c83f1d9e32e03ee42750c6b
lucien lin <lucienlin@luciendeMacBook-Pro.local>
2023-11-23 01:23:35 +0800

    haha

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 import streamlit as st
 import random
 import time
-from pygame import mixer
 
 MEMBERS = "Lucien, Sam, Patty, Henry, Johnny, Wendy, Mason, Aki"
 
-def play_audio():
-    mixer.init()
-    mixer.music.load("fall.mp3")
-    mixer.music.play()
 
 def assign_members_to_themes(members, themes):
     """Assign members to themes"""
@@ -18,8 +13,10 @@ def assign_members_to_themes(members, themes):
     theme_dict = {}
     random.shuffle(members)
     for i in range(len(themes)):
-        theme_dict[themes[i]] = members[i*num_members_per_theme : (i+1)*num_members_per_theme]
-    remaining_members = members[len(themes)*num_members_per_theme:]
+        theme_dict[themes[i]] = members[
+            i * num_members_per_theme : (i + 1) * num_members_per_theme
+        ]
+    remaining_members = members[len(themes) * num_members_per_theme :]
     random.shuffle(remaining_members)
     for i, member in enumerate(remaining_members):
         theme_dict[themes[i]].append(member)
@@ -28,12 +25,17 @@ def assign_members_to_themes(members, themes):
 
 def interface():
     """Show streamlit interface"""
-    
-    st.markdown("<h1 style='text-align: center; color: orange;'>🎁 聖誕禮物主題分配 🎁</h1>", unsafe_allow_html=True)
+
+    st.markdown(
+        "<h1 style='text-align: center; color: orange;'>🎁 聖誕禮物主題分配 🎁</h1>",
+        unsafe_allow_html=True,
+    )
     st.markdown("<div style='font-size: 20px;'>參與人員</div>", unsafe_allow_html=True)
 
     members = st.text_area("", value=MEMBERS)
-    st.markdown("<div style='font-size: 20px;'>主題名稱(每行一個主題)</div>", unsafe_allow_html=True)
+    st.markdown(
+        "<div style='font-size: 20px;'>主題名稱(每行一個主題)</div>", unsafe_allow_html=True
+    )
     themes_input = st.text_area("")
 
     if st.button("隨機分組"):
@@ -42,7 +44,6 @@ def interface():
         else:
             themes = themes_input.split("\n")
             members = members.split(",")
-            play_audio()
             gif_placeholder = st.empty()
             gif_placeholder.image("angry-birds.gif")
             time.sleep(2)
@@ -52,4 +53,3 @@ def interface():
 
 if __name__ == "__main__":
     interface()
-
